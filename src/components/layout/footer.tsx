30107588a9fce10e39ecf19db70ea9a3599ee497
tkznzl12 <74891586+tkznzl12@users.noreply.github.com>
2026-05-12 22:43:33 +0900

    feat: footer 구조, css 작성 완료
    - mock데이터로 임의로 삽입
    - 추후 상황에 따라 내부 content가 달라질 수 있음

--- src/components/layout/footer.tsx
+++ src/components/layout/footer.tsx
@@ -1,45 +1,55 @@
 import styles from './footer.module.scss';
 import genreList from '../../data/ganre';
+import Logo from '../../assets/logo.svg';
 export default function Footer() {
   return (
     <footer className={styles.footer}>
-      <div className={styles.footerContent}>
-        <div className="footer-content-item">
-          <p>국내 모든 공연 정보를 한 곳에서.</p>
-          <p> AI와 함께 나만의 공연을 찾아보세요.</p>
-        </div>
-        <div className="footer-content-item">
-          <p className="footer-content-title">공연 카테고리</p>
-          {genreList.map((genre, index) => (
-            <a href="" key={`footer-genre-${index}`}>
-              {genre.name}
-            </a>
-          ))}
-        </div>
-        <div className="footer-content-item">
-          <p className="footer-content-title">서비스</p>
-          {genreList.map((genre, index) => (
-            <a href="" key={`footer-genre-${index}`}>
-              {genre.name}
-            </a>
-          ))}
-        </div>
-        <div className="footer-content-item">
-          <p className="footer-content-title">고객지원</p>
-          <div>
-            <p>이메일: support@culturepick.kr</p>
-            <p>운영시간: 평일 10:00~18:00</p>
+      <div className={styles.footerContainer}>
+        <div className={styles.footerContent}>
+          <div className={styles.footerContentItem}>
+            <span>
+              <img src={Logo.src} alt="로고" />
+            </span>
+            <p>국내 모든 공연 정보를 한 곳에서.</p>
+            <p> AI와 함께 나만의 공연을 찾아보세요.</p>
+          </div>
+          <div className={styles.footerContentItem}>
+            <p className={styles.footerContentTitle}>공연 카테고리</p>
+            <div className={styles.footerContentList}>
+              {genreList.map((genre, index) => (
+                <a href="" key={`footer-genre-${index}`}>
+                  {genre.name}
+                </a>
+              ))}
+            </div>
+          </div>
+          <div className={styles.footerContentItem}>
+            <p className={styles.footerContentTitle}>서비스</p>
+            <div className={styles.footerContentList}>
+              {genreList.map((genre, index) => (
+                <a href="" key={`footer-genre-${index}`}>
+                  {genre.name}
+                </a>
+              ))}
+            </div>
+          </div>
+          <div className={styles.footerContentItem}>
+            <p className={styles.footerContentTitle}>고객지원</p>
+            <div className={styles.footerContentList}>
+              <p>이메일: support@culturepick.kr</p>
+              <p>운영시간: 평일 10:00~18:00</p>
+            </div>
           </div>
         </div>
-      </div>
-      <div className={styles.footerBottom}>
-        <p className="footer-bottom-left">
-          © 2026 컬처픽. All rights reserved.
-        </p>
-        <div>
-          <p>이용약관</p>
-          <p>개인정보 처리방침</p>
-          <p>사업자 정보</p>
+        <div className={styles.footerBottom}>
+          <p className={styles.footerCaption}>
+            © 2026 컬처픽. All rights reserved.
+          </p>
+          <div className={styles.footerTerms}>
+            <p>이용약관</p>
+            <p>개인정보 처리방침</p>
+            <p>사업자 정보</p>
+          </div>
         </div>
       </div>
     </footer>
