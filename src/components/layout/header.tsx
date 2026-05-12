0fa7c69c6afecf23c107e0a23d65ec3fbb7c3465
jungsujin <tkznzl12@naver.com>
2026-05-12 14:21:09 +0900

    feat: 초기 CSS 세팅 및 레이아웃 test - global.css 설정 - header(Nav) components 제작 후 main 출력 테스트

---
+++ src/components/layout/header.tsx
@@ -0,0 +1,9 @@
+export default function Header(){
+    return (
+        <nav>
+            <div className="nav-container">
+            <div className="nav-left-">이것은 nev 우측</div>
+            </div>
+        </nav>
+    )
+}
