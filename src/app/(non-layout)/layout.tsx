e09b05239e37c6a16fc24ac498fec2c016d520c0
jungsujin <tkznzl12@naver.com>
2026-05-18 13:55:50 +0900

    feat: 배경화면 gradient 요소 추가 - nonlayout page(login, signup)의 배경 요소 추가 - overflow hidden data 추가

--- src/app/(non-layout)/layout.tsx
+++ src/app/(non-layout)/layout.tsx
@@ -1,7 +1,14 @@
-export default function RootLayout({
+import styles from './styles/non-layout.module.scss';
+export default function NonLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  return <>{children}</>;
+  return (
+    <div className={styles.nonLayout}>
+      <div className={styles.gradientTop} />
+      {children}
+      <div className={styles.gradientBottom} />
+    </div>
+  );
 }
